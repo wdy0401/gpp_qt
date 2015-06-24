508221f27f869d2e510305ce58dd74cbf209fdb6
wdy0401 <wdy0401@gmail.com>
2015-06-24 15:56:02 +0800

    增添wmath

---
+++ wfunction/wmath.cpp
@@ -0,0 +1,32 @@
+#include"wmath.h"
+
+using namespace std;
+
+double wmath::mean(list<double> * nowlist)
+{
+    if(nowlist->size()==0)
+    {
+        return 0;
+    }
+    double total=0;
+    for(list<double>::iterator iter=nowlist->begin() ; iter!=nowlist->end() ; iter++)
+    {
+        total+=*iter;
+    }
+    return total/nowlist->size();
+
+}
+double wmath::stdev(list<double> * nowlist)
+{
+    if(nowlist->size()==0)
+    {
+        return 0;
+    }
+    double m=mean(nowlist);
+    double total=0;
+    for(list<double>::iterator iter=nowlist->begin() ; iter!=nowlist->end() ; iter++)
+    {
+        total+=pow((m-*iter),2);
+    }
+    return sqrt(total/(nowlist->size()-1));
+}
---
+++ wfunction/wmath.h
@@ -0,0 +1,11 @@
+#ifndef WMATH
+#define WMATH
+#include<list>
+#include<math.h>
+class wmath
+{
+public:
+    static double mean(std::list<double> *);
+    static double stdev(std::list<double> *);
+};
+#endif
