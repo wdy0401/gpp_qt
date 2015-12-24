285be7681a1808f94f185376cd90c32083018836
wdy0401 <wdy0401@gmail.com>
2015-12-24 16:57:58 +0800

    添加了wtimer的使用方法

--- wtimer/wtimer.h
+++ wtimer/wtimer.h
@@ -2,12 +2,14 @@
 #define WTIMER
 
 //根据数据输入数据设定时间
+//方式1 20151224.09 15 00 500
+//方式2 将日期设为0 也就是均为0 1之间的小数
 
 class wtimer
 {
 public:
-	double nowtic(){return _nowtic;};
-	void settic(double nt){_nowtic =nt;};
+    double nowtic(){return _nowtic;}
+    void settic(double nt){_nowtic =nt;}
 private:
 	double _nowtic;
 };
